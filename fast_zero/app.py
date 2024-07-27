7c8a3df7d8cdd49ca31d497d7176531c3c0c0d4f
josedembo <josedembo18@aluno.unilab.edu.br>
2024-07-27 18:28:38 -0300

    feat: Adiciona novas rotas de CRUD

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,46 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema, UsersSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get('/')
+
+@app.get('/', response_model=Message)
 def read_root():
     return {'message': 'Hello world'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_html():
+    return """<html>
+        <head>
+            <title>
+                Olá Mundo
+            </title>
+        </head>
+        <body>
+            <h1>
+                Olá Mundo
+            <h1>
+        </body>
+    </html>"""
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(
+        id=len(database) + 1,
+        **user.model_dump()
+    )
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get("/users", response_model=UsersSchema)
+def get_users():
+    return {"users": database}
